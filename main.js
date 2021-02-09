c45fda22ccc6fad253bb96c488c8b5aa372baa6a
cdiazit5 <cdiazit5@gmail.com>
2021-02-09 10:54:51 -0500

    check point, working on edit func to swithc back to not watched

--- main.js
+++ main.js
@@ -5,36 +5,41 @@ let allMovieContainer = document.querySelector('.allMovies')
 let movies = document.querySelectorAll('.movie')
 
 // Event Listeners
-form.addEventListener ('submit', function(e) {
+form.addEventListener('submit', function (e) {
     e.preventDefault()
     addMovie()
 })
 
 document.addEventListener('click', e => {
-    if (e.target.className ==="movie-watched") {
+    if (e.target.className === "movie-watched") {
+
         if (e.target.innerHTML !== 'watched') {
-        e.target.innerHTML = "watched"
-        let finishedMovieDate = document.createElement('p')
-        let date = moment().format("MMM Do YY")
-        finishedMovieDate.innerHTML = date
-        e.target.parentElement.appendChild(finishedMovieDate)
-        editWatched(e.target, date)
+            e.target.innerHTML = "Watched"
+            let finishedMovieDate = document.createElement('p')
+            let date = moment().format("MMM Do YY")
+            finishedMovieDate.innerHTML = date
+            e.target.parentElement.appendChild(finishedMovieDate)
+            editWatched(e.target, date)
         } else {
             e.target.innerHTML = 'not watched'
         }
+
+    } else if (e.target.className === "delete-button") {
+        deleteMovie(e.target)
+
     }
-    
+
 })
 
 // for (let movie of movies) {
 //     movie.addEventListener('click', e => {
 //         console.log('movie clicked') 
 //     })
-    
+
 // }
 
 function listAllMovies() {
-    fetch (url)
+    fetch(url)
         .then(res => res.json())
         .then(data => {
             console.log(data[0].title)
@@ -45,50 +50,63 @@ function listAllMovies() {
 }
 
 function addMovie() {
-    
+
     let today = new Date();
     let movieTitle = document.querySelector('input').value
-    let watched = false;
+    let watched_at = null;
 
 
-    fetch (url,  { 
+    fetch(url, {
         method: 'POST',
-        headers: { 'Content-Type':'application/json'},
-        body: JSON.stringify ({
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
             title: movieTitle,
-        watched: watched,
-        // created_at: today 
-        })})
-        .then (res => res.json ())
-        .then (data => {
+            watched_at: watched_at
+            // watched: watched,
+            // created_at: today 
+        })
+    })
+        .then(res => res.json())
+        .then(data => {
             console.log(data)
         })
 }
 
-function editWatched (element, time) {
+function editWatched(element, time) {
     const movieId = element.parentElement.id
-    fetch (`http://localhost:3000/movies/${movieId}`, {
+
+    fetch(`http://localhost:3000/movies/${movieId}`, {
         method: 'PATCH',
-        headers: { 'Content-Type' : 'application/json' },
-        body: JSON.stringify ({
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
             watched_at: time
         })
     })
-    .then(res => res.json())
-    .then(data => {
-        console.log(data)
-        // renderMovie(element.parentElement, data)
-        // listAllMovies()
-    })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+            // renderMovie(element.parentElement, data)
+            // listAllMovies()
+        })
 }
 
+function deleteMovie(movie) {
+    let movieId = movie.parentElement.id
+    fetch(url + movieId, { method: 'DELETE' })
+        .then(() => {
+            movie.parentElement.remove()
+        })
+}
 
-
-function renderMovie(movie){
+function renderMovie(movie) {
     let movieDiv = document.createElement('div')
     movieDiv.className = 'movie'
     movieDiv.id = movie.id
     let movieEl = document.createElement('p')
+    let delMovieButton = document.createElement('button')
+    delMovieButton.className = "delete-button"
+    delMovieButton.innerHTML = "Delete"
+
     let movieWatched = document.createElement('div')
     movieWatched.className = "movie-watched"
     movieWatched.innerHTML = "Not Watched"
@@ -104,12 +122,17 @@ function renderMovie(movie){
 
     movieDiv.appendChild(movieEl)
     movieDiv.appendChild(movieWatched)
+    movieDiv.appendChild(delMovieButton)
     // movieDiv.appendChild(movieDate)
 
     allMovieContainer.appendChild(movieDiv)
 }
 
 listAllMovies()
+
+
+
+
 
 
 
